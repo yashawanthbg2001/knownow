e78a5e14bb26102b76ab85169eef0e7ec9adf1fb
yashawanthbg2001 <yashawanthbg@gmail,com>
2026-01-25 11:59:57 +0530

    proceess.env is add for index and slug new 1 brute fprce 21

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'static', // Ensure this is set to static
+  output: 'static',
+  // This helps Astro understand the environment
+  env: {
+    schema: {
+      TURSO_DATABASE_URL: { type: 'string', context: 'server', access: 'secret' },
+      TURSO_AUTH_TOKEN: { type: 'string', context: 'server', access: 'secret' },
+    }
+  }
 });
