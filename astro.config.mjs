541c128826711fe15ba235fda643bea918681b7c
yashawanthbg2001 <yashawanthbg@gmail,com>
2026-01-25 22:03:12 +0530

    index new edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://yashawanthbg2001.github.io',
-  base: '/knownow', // Only if your repo is named 'knownow'
+  base: '/knownow',
   output: 'static',
 });
